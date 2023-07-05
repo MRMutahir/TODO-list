898442e763392d7f0cd165324a5fbc67146aea16
Mhummad Mutahir <mutahirkareem820@gmail.com>
2023-07-06 01:08:17 +0500

    done

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 let inputvalue = document.getElementById('inputvalue');
-// console.log(inputvalue);
 let addBtn = document.getElementById('addBtn');
-// console.log(addBtn);
 let listName = document.querySelector('.listName');
-// console.log(listName)
 let li = document.querySelector('li');
-// console.log(li);
 let screen1 = document.querySelector('.screen1');
+// console.log(addBtn);
+// console.log(listName)
+// console.log(inputvalue);
+// console.log(li);
 // console.log(screen1);
 addBtn.addEventListener('click', () => {
-    if (inputvalue.value == ' ') {
-        alert('sorry')
-
+    if (inputvalue.value == '') {
+        alert('Enter some value')
     } else {
-        // span.innerText = screen1;
-
         let li = document.createElement('li');
         li.innerHTML = inputvalue.value;
         listName.appendChild(li).className = ' lisingleValue'
-       
         let span = document.createElement('span');
         li.appendChild(span)
         span.appendChild(li.firstChild)
+        console.log(li)
         let delet = document.createElement('button');
         delet.innerText = 'delet';
         li.appendChild(delet).className = ' dBtn';
         delet.className += ' same'
         delet.addEventListener('click', () => {
-            // delet.className += ' same'
             li.innerText = ''
         })
         let edit = document.createElement('button');
@@ -35,41 +31,10 @@ addBtn.addEventListener('click', () => {
         li.appendChild(edit).className = ' eBtn'
         edit.className += ' same'
         edit.addEventListener('click', () => {
-            // console.log(li);
-            // span.innerText = screen1.innerHTML
-            // console.log(span)
-            // li = screen1.innerHTML
-            // li.firstChild = inputvalue.style.backgroundColor = "green"
-            // li.firstChild = inputvalue.style.backgroundColor = "green"
-            // var a_li = li
-            inputvalue.value  = span.innerText;
-        //    span.innerHTML = span.innerText
-        if (inputvalue.value == span.innerText) {
-            li.innerHTML = ''
-            addBtn.innerText = 'Edit'
-            addBtn.style.color = "blue"
-            // console.log('hi')
-            // inputvalue.value  = span.innerHTML;
-            // edit.addEventListener('click',()=>{
-
-            // })
-            
-            
-        }else{
-            // inputvalue.value = span.innerText
-            // addBtn.innerText="ADD"
-            console.log('hi')
-        }
-
-
+            inputvalue.value = span.innerText;
+            li.innerHTML = ""
         })
-        inputvalue.value = ' '
     }
 
+    inputvalue.value = ''
 })
-
-
-
-
-    // console.log(delet)
-    // console.log(edit)
